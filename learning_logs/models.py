f701306188f5d66f2e32c99926de3f64642e4da8
pramodavuland <pramod.avula.-nd@disney.com>
2024-03-27 15:53:36 -0500

    Defining Entry Model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,6 +7,19 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
+class Entry(models.Model):
+    """ SOmething specific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self) :
+        """ Return a simple  string representing the entry"""
+        return f"{self.text[:50]}..."
+
 def __str__(self):
     """ Return a string representation of the model."""
     return self.text
